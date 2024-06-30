aff378f20471fcc1af8a39b0ac8d776ebb8a3ec2
Matheus Ramos <geof.matheus@gmail.com>
2024-06-30 20:03:22 -0300

    addd

--- src/components/RenderMarkdown.js
+++ src/components/RenderMarkdown.js
@@ -1,11 +1,12 @@
 import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
+import rehypeHighlight from 'rehype-highlight'
 
 const _mapProps = (props) => ({
   ...props,
 
 });
 
-const RenderMarkdown = (props) => <ReactMarkdown rehypePlugins={[rehypeRaw]} {..._mapProps(props)} />
+const RenderMarkdown = (props) => <ReactMarkdown rehypePlugins={[rehypeHighlight]} {..._mapProps(props)} />
 
 export default RenderMarkdown
