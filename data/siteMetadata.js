ee27621f1bf21dd29ffaf0b1e7cfbef490bbd14d
Matheus Ramos <geof.matheus@gmail.com>
2024-07-01 21:24:00 -0300

    modify site meta data

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,10 +6,10 @@ const siteMetadata = {
   description: 'My Blog',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://www.matheusramos.blog/',
+  siteRepo: 'https://github.com/MatheusRamo/matheusRamos',
   siteLogo: '/static/images/logo.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/logo.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'geof.matheus@gmail.com',
   github: 'https://github.com/MatheusRamo',
